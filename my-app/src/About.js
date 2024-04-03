859dc04ce2c729d3d4196fdee611808866b999c6
Jinoma-Star <antoine.rami@hotmail.fr>
2024-04-03 04:31:00 +0100

    Mise en place Routeur & Sass

---
+++ my-app/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>À Propos</h1>
+      <p>Ce site est conçu pour démontrer l'utilisation de React Router.</p>
+    </div>
+  );
+};
+
+export default About;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="App">
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Accueil</Link>
+          <Link to="/about">À propos</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+</div>
+);
 }
 
 export default App;
