be2d1c5d611d9bb763ce3f9e0ab535fd4e6053db
Jinoma-Star <antoine.rami@hotmail.fr>
2024-04-17 02:47:13 +0100

    Correction style

--- my-app/src/Components/Banner.js
+++ my-app/src/Components/Banner.js
@@ -5,7 +5,6 @@ function Banner({ image, texte }) {
   return (
     <div className="banner">
       <img className="banner__img" src={image} alt="Bannière" />
-      <div className="banner__sombre"></div>
       <span className="banner__txt">{texte}</span>
     </div>
   );
